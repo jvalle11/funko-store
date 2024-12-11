f396dbd91755f70839c37c833d9c0b45dc3dc7dd
jvalle11 <jvalle@arcadia.edu>
2024-12-10 19:08:41 -0500

    netlify api change for vercel

--- src/components/CartOverview.js
+++ src/components/CartOverview.js
@@ -31,7 +31,7 @@ const Cart = () => {
         return;
       }
 
-      const response = await fetch('/.netlify/functions/createCheckoutSession', {
+      const response = await fetch('/.api/functions/createCheckoutSession', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
